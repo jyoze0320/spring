3a4ed8fb30c5bff33f17a6a8bfe1f692c336cabe
heojk <hjk7902@gmail.com>
2022-12-28 17:59:28 +0900

    처음 시작하는 스프링 프레임워크 [2021 개정판]

--- MultiBoard/src/main/java/com/example/myapp/board/controller/BoardController.java
+++ MultiBoard/src/main/java/com/example/myapp/board/controller/BoardController.java
@@ -16,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -87,7 +88,7 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value="/board/write", method=RequestMethod.POST)
-	public String writeArticle(Board board, RedirectAttributes redirectAttrs) {
+	public String writeArticle(Board board, BindingResult results, RedirectAttributes redirectAttrs) {
 		logger.info("/board/write : " + board.toString());
 		try{
 			board.setContent(board.getContent().replace("\r\n", "<br>"));
